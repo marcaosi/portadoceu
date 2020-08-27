78b6ddffe2b652d3872074d9f69f8dbeba37b229
Marcos <marcaosi2014@gmail.com>
2020-08-26 23:43:59 -0300

    Updates

---
+++ precache-manifest.d7c33940039fbbe592d54753cb98370d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba5502aceffb20794adde8ed6a0c0a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e97d071008c2402414c5",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6fef9de1d6a629629679",
+    "url": "/static/css/main.e8131677.chunk.css"
+  },
+  {
+    "revision": "e97d071008c2402414c5",
+    "url": "/static/js/2.6161e693.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6161e693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fef9de1d6a629629679",
+    "url": "/static/js/main.1147667d.chunk.js"
+  },
+  {
+    "revision": "f5bdc5e1d72ddca21be0",
+    "url": "/static/js/runtime-main.59b1432c.js"
+  },
+  {
+    "revision": "5f09e183dd45f8297da93a6919faa4bd",
+    "url": "/static/media/banner.5f09e183.jpg"
+  },
+  {
+    "revision": "6096db252a2b5e8ad5ce0621a3243f0c",
+    "url": "/static/media/logo.6096db25.svg"
+  },
+  {
+    "revision": "3a3e4514aebd4c5eefcd107e5b12704c",
+    "url": "/static/media/novidades.3a3e4514.png"
+  },
+  {
+    "revision": "b7653dd2a09e01d4bf1a5a51cc9fac6a",
+    "url": "/static/media/novidades2.b7653dd2.png"
+  },
+  {
+    "revision": "8b96a2f5e87f135074136cd2189679af",
+    "url": "/static/media/novidades3.8b96a2f5.png"
+  },
+  {
+    "revision": "78973f74439f82820190eefed92cada2",
+    "url": "/static/media/sobre.78973f74.jpg"
+  }
+]);
